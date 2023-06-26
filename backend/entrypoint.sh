b50cecb6a6783b06aa62469263843aed42505682
George Prokofev <prokofev987@mail.ru>
2023-06-26 15:45:46 +0300

    Fixed

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,10 +8,9 @@ done
 
 echo "Postgres started"
 
-gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
 python manage.py migrate
 python manage.py collectstatic
 cp -r /app/collected_static/. /static/static/
-
+gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
 
 exec "$@"
